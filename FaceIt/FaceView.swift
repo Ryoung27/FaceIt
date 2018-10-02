3b43df104b16226030cfc9195126cd3e4532c370
Ryoung27 <RRYoung89@gmail.com>
2018-10-02 07:22:27 -0500

    face up and running, also added to storyboard with Designable

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
-
+@IBDesignable
 class FaceView: UIView {
 
     
     var scale: CGFloat = 0.9
     
     var eyesOpen: Bool = false
+    
+    var mouthCurvature: Double = 1.0 //1.0 is full smile // -1.0 is full frown
     
     private var skullRadius: CGFloat {
         
@@ -56,7 +58,28 @@ class FaceView: UIView {
         return path
     }
     
-    private func pathForMouth()
+    private func pathForMouth() -> UIBezierPath{
+        let mouthWidth = skullRadius / Ratios.skullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.skullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratios.skullRadiusTOEyeOffset
+        
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth / 2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        
+        
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+        
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 3, y: start.y + smileOffset)
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 3, y: start.y + smileOffset)
+        
+        let path = UIBezierPath(rect: mouthRect)
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 5.0
+        return path
+    }
     
     private func pathForSkull() -> UIBezierPath{
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
@@ -71,6 +94,7 @@ class FaceView: UIView {
         pathForSkull().stroke()
         pathForEye(.left).stroke()
         pathForEye(.right).stroke()
+        pathForMouth().stroke()
     }
     private struct Ratios {
         static let skullRadiusTOEyeOffset: CGFloat = 3
